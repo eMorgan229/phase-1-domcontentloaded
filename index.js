a39febe1c1a40439391dc4396e50f0aeafe18a5d
Em <ekramm@me.com>
2022-05-02 17:57:44 -0400

    commit tree 57a813e6b601b28b79d9d87bc05ebd4fc94aa43c

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function(e) {
+    const getText = document.getElementById('text');
+    console.log(getText)
+    getText.innerHTML = 'This is really cool!'
+});
+
+
